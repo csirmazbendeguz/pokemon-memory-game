51ebbf4536718c4c22b53aea289aab7083d11126
bendeguz.csirmaz <bendeguz.csirmaz@gamma.co.uk>
2019-10-11 15:19:33 +0200

    Add test for tries counter handler

---
+++ src/test/java/net/csirmazbendeguz/memory_game/unit/state/event_handlers/TriesCounterHandlerTest.java
@@ -0,0 +1,44 @@
+package net.csirmazbendeguz.memory_game.unit.state.event_handlers;
+
+import net.csirmazbendeguz.memory_game.event.objects.GameStartEvent;
+import net.csirmazbendeguz.memory_game.event.objects.PairFlipUpEvent;
+import net.csirmazbendeguz.memory_game.state.TriesCounter;
+import net.csirmazbendeguz.memory_game.state.event_handlers.TriesCounterHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TriesCounterHandlerTest {
+
+    @Mock
+    private TriesCounter mockTriesCounter;
+
+    private TriesCounterHandler handler;
+
+    @BeforeEach
+    void setup() {
+        handler = new TriesCounterHandler(mockTriesCounter);
+    }
+
+    @Test
+    void testResetOnNewGame() {
+        GameStartEvent mockEvent = mock(GameStartEvent.class);
+        handler.gameStarted(mockEvent);
+        verifyZeroInteractions(mockEvent);
+        verify(mockTriesCounter).reset();
+    }
+
+    @Test
+    void testIncrementOnFlipUp() {
+        PairFlipUpEvent mockEvent = mock(PairFlipUpEvent.class);
+        handler.pairFlippedUp(mockEvent);
+        verifyZeroInteractions(mockEvent);
+        verify(mockTriesCounter).increment();
+    }
+
+}
